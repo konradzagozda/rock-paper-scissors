4f9ebce7a6c73d3a05a2b962add30b1f0313508b
Konrad <konradz1369@gmail.com>
2019-05-24 01:27:29 +0200

    add graphical interface

--- script.js
+++ script.js
@@ -23,29 +23,31 @@ function singleRound(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  let round = 0;
-  let playerScore = 0;
-  let computerScore = 0;
-  let draws = 0;
-  while (round < 5) {
-    let playerMove = prompt('Tell me your pick ( rock / paper / scissors)');
-    let result = singleRound(playerMove, computerPlay());
-    if (result === 'win') {
-      playerScore++;
-      console.log('you win');
-    } else if (result === 'lose') {
-      console.log('you lose');
-      computerScore++;
-    } else {
-      console.log('draw');
-      draws++;
-    }
-    round++;
-  }
-  let winner = playerScore > computerScore ? 'player' : playerScore === computerScore ? 'no one' : 'computer';
-  console.log('And the winner is... ', winner);
-  return winner;
+let imgs = document.querySelectorAll('img');
+let player_move = null;
+let playerScore = document.querySelector('#player');
+let computerScore = document.querySelector('#computer');
+let draws = document.querySelector('#draws')
+let message = document.querySelector('#current-message');
+
+
+
+function playRound(){
+  player_move = this.alt;
+  let result = singleRound(player_move, computerPlay());
+  if (result === 'win') {
+        playerScore.textContent++;
+        message.textContent = "You Win!";
+      } else if (result === 'lose') {
+        message.textContent = "You Lose!";
+        computerScore.textContent++;
+      } else {
+        message.textContent = "Tie!";
+        draws.textContent++;
+      }
 }
 
-game();
+imgs.forEach(x => x.addEventListener('click', playRound));
+
+
+// @TODO: gradient changes depending on results. (the better result -> greener screen)
